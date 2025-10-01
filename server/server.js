9850ff98f858c06c64ead46a4b6f82041a32ea55
Yasaswini <yasaswini.gaddam21@gmail.com>
2025-10-01 15:14:47 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -15,7 +15,11 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.use(cors());
+app.use(cors({
+  origin: "*",  
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 
 app.use('/api/sessions', sessionRoutes);
